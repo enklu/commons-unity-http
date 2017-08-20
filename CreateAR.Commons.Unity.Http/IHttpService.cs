457c01f041e9225711d8b5363f030653c5a9a4f2
Benjamin Jordan <bmj465@gmail.com>
2017-08-19 21:41:57 -0700

    Working on storage service.

--- CreateAR.Commons.Unity.Http/IHttpService.cs
+++ CreateAR.Commons.Unity.Http/IHttpService.cs
@@ -1,9 +1,98 @@
 ﻿using System.Collections.Generic;
 using CreateAR.Commons.Unity.Async;
 using CreateAR.Commons.Unity.DataStructures;
+using JetBrains.Annotations;
 
 namespace CreateAR.Commons.Unity.Http
 {
+    public class UrlBuilder
+    {
+        public string BaseUrl = "localhost";
+        public string Protocol = "http";
+        public int Port = 80;
+        public string Version = "";
+
+        public readonly List<Tuple<string, string>> Replacements = new List<Tuple<string, string>>();
+
+        public string Url(string endpoint)
+        {
+            if (string.IsNullOrEmpty(Version))
+            {
+                return string.Format("{0}{1}:{2}/{3}",
+                    FormatProtocol(),
+                    FormatBaseUrl(),
+                    FormatPort(),
+                    FormatEndpoint(endpoint));
+            }
+
+            return string.Format("{0}{1}:{2}/{3}/{4}",
+                FormatProtocol(),
+                FormatBaseUrl(),
+                FormatPort(),
+                FormatVersion(),
+                FormatEndpoint(endpoint));
+        }
+
+        private string FormatProtocol()
+        {
+            var protocol = string.IsNullOrEmpty(Protocol)
+                ? "http://"
+                : Protocol;
+
+            if (!protocol.EndsWith("://"))
+            {
+                protocol += "://";
+            }
+
+            return protocol;
+        }
+
+        private string FormatBaseUrl()
+        {
+            var baseUrl = string.IsNullOrEmpty(BaseUrl)
+                ? "localhost"
+                : BaseUrl;
+
+            baseUrl = baseUrl.Trim('/');
+
+            return baseUrl;
+        }
+
+        private int FormatPort()
+        {
+            return Port > 0
+                ? Port
+                : 80;
+        }
+
+        private string FormatVersion()
+        {
+            return Version.Trim('/');
+        }
+
+        private string FormatEndpoint(string endpoint)
+        {
+            if (string.IsNullOrEmpty(endpoint))
+            {
+                return "";
+            }
+
+            endpoint = endpoint.Trim('/');
+
+            // replacements
+            for (int i = 0, len = Replacements.Count; i < len; i++)
+            {
+                var replacement = Replacements[i];
+
+                endpoint = endpoint.Replace(
+                    "{" + replacement.Item1 + "}",
+                    replacement.Item2);
+            }
+
+            return endpoint;
+        }
+    }
+
     /// <summary>
     /// Verbs.
     /// </summary>
