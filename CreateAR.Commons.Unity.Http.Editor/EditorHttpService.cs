572d576db25bda7b764016b7c0a8de6531d6faca
Benjamin Jordan <bmj465@gmail.com>
2018-01-09 12:15:47 -0800

    Reimplemented EditorHttpService for a 3rd time, here using old WWW.

--- CreateAR.Commons.Unity.Http.Editor/EditorHttpService.cs
+++ CreateAR.Commons.Unity.Http.Editor/EditorHttpService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Net;
+using System.Linq;
 using System.Text;
 using CreateAR.Commons.Unity.Async;
 using CreateAR.Commons.Unity.DataStructures;
 using CreateAR.Commons.Unity.Logging;
+using UnityEngine;
 
 namespace CreateAR.Commons.Unity.Http.Editor
 {
@@ -14,41 +16,6 @@ namespace CreateAR.Commons.Unity.Http.Editor
     public class EditorHttpService : IHttpService
     {
         /// <summary>
-        /// How to deserialize.
-        /// </summary>
-        protected enum SerializationType
-        {
-            Json,
-            Raw
-        }
-
-        /// <summary>
-        /// State passed along with the <c>HttpWebRequest</c> async API.
-        /// </summary>
-        protected class HttpState
-        {
-            /// <summary>
-            /// Url.
-            /// </summary>
-            public string Url;
-
-            /// <summary>
-            /// Request.
-            /// </summary>
-            public HttpWebRequest Request;
-
-            /// <summary>
-            /// Callback.
-            /// </summary>
-            public Action<object> Resolve;
-
-            /// <summary>
-            /// Serialization strategy.
-            /// </summary>
-            public SerializationType Serialization;
-        }
-
-        /// <summary>
         /// Specifies content types.
         /// </summary>
         private const string CONTENT_TYPE_JSON = "application/json";
@@ -57,7 +24,12 @@ namespace CreateAR.Commons.Unity.Http.Editor
         /// Serializer.
         /// </summary>
         private readonly ISerializer _serializer;
-        
+
+        /// <summary>
+        /// Bootstrapper.
+        /// </summary>
+        private readonly IBootstrapper _bootstrapper;
+
         /// <inheritdoc cref="IHttpService"/>
         public UrlBuilder UrlBuilder { get; private set; }
 
@@ -67,10 +39,13 @@ namespace CreateAR.Commons.Unity.Http.Editor
         /// <summary>
         /// Constructor.
         /// </summary>
-        public EditorHttpService(ISerializer serializer)
+        public EditorHttpService(
+            ISerializer serializer,
+            IBootstrapper bootstrapper)
         {
             _serializer = serializer;
-            
+            _bootstrapper = bootstrapper;
+
             UrlBuilder = new UrlBuilder();
             Headers = new List<Tuple<string, string>>();
         }
@@ -78,13 +53,13 @@ namespace CreateAR.Commons.Unity.Http.Editor
         /// <inheritdoc cref="IHttpService"/>
         public void Abort()
         {
-            
+
         }
 
         /// <inheritdoc cref="IHttpService"/>
         public IAsyncToken<HttpResponse<T>> Get<T>(string url)
         {
-            return SendJsonRequest<T>(HttpVerb.Get, url, null);
+            return SendRequest<T>(HttpVerb.Get, url, null);
         }
 
         /// <inheritdoc cref="IHttpService"/>
@@ -92,19 +67,19 @@ namespace CreateAR.Commons.Unity.Http.Editor
             string url,
             object payload)
         {
-            return SendJsonRequest<T>(HttpVerb.Post, url, payload);
+            return SendRequest<T>(HttpVerb.Post, url, payload);
         }
 
         /// <inheritdoc cref="IHttpService"/>
         public IAsyncToken<HttpResponse<T>> Put<T>(string url, object payload)
         {
-            return SendJsonRequest<T>(HttpVerb.Put, url, payload);
+            return SendRequest<T>(HttpVerb.Put, url, payload);
         }
 
         /// <inheritdoc cref="IHttpService"/>
         public IAsyncToken<HttpResponse<T>> Delete<T>(string url)
         {
-            return SendJsonRequest<T>(HttpVerb.Delete, url, null);
+            return SendRequest<T>(HttpVerb.Delete, url, null);
         }
 
         /// <inheritdoc cref="IHttpService"/>
@@ -122,18 +97,10 @@ namespace CreateAR.Commons.Unity.Http.Editor
         /// <inheritdoc cref="IHttpService"/>
         public IAsyncToken<HttpResponse<byte[]>> Download(string url)
         {
-            var token = new AsyncToken<HttpResponse<byte[]>>();
-
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            
-            ApplyHeaders(Headers, request);
-            
-            ProcessResponse(
-                (HttpWebResponse) request.GetResponse(),
-                token,
-                SerializationType.Raw);
-
-            return token;
+            return SendRequest<byte[]>(
+                HttpVerb.Get,
+                url,
+                null);
         }
 
         /// <summary>
@@ -144,30 +111,31 @@ namespace CreateAR.Commons.Unity.Http.Editor
         /// <param name="url">Complete endpoint url.</param>
         /// <param name="payload">Object to send.</param>
         /// <returns></returns>
-        private IAsyncToken<HttpResponse<T>> SendJsonRequest<T>(
+        private IAsyncToken<HttpResponse<T>> SendRequest<T>(
             HttpVerb verb,
             string url,
             object payload)
         {
-            var token = new AsyncToken<HttpResponse<T>>();
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.ContentType = CONTENT_TYPE_JSON;
-            request.Method = verb.ToString().ToUpperInvariant();
-            
-            ApplyHeaders(Headers, request);
-            ApplyJsonPayload(payload, request);
+            Log.Info(this,
+                "{0} {1}",
+                verb.ToString().ToUpperInvariant(),
+                url);
 
-            // block!
-            try
+            var token = new AsyncToken<HttpResponse<T>>();
+
+            byte[] bytes = null;
+            if (null != payload)
             {
-                var response = (HttpWebResponse) request.GetResponse();
-                ProcessResponse(response, token, SerializationType.Json);
+                _serializer.Serialize(payload, out bytes);
             }
-            catch (Exception exception)
-            {
-                token.Fail(exception);
-            }
-            
+
+            var request = new WWW(
+                url,
+                bytes,
+                HeaderDictionary());
+
+            _bootstrapper.BootstrapCoroutine(Wait(request, token));
+
             return token;
         }
 
@@ -175,143 +143,120 @@ namespace CreateAR.Commons.Unity.Http.Editor
         /// Sycnhronously process response.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="response">Response object.</param>
+        /// <param name="request">WWW request.</param>
         /// <param name="token">Token to resolve.</param>
-        /// <param name="serialization">Serialization.</param>
-        private void ProcessResponse<T>(
-            HttpWebResponse response,
-            AsyncToken<HttpResponse<T>> token,
-            SerializationType serialization)
+        private IEnumerator Wait<T>(
+            WWW request,
+            AsyncToken<HttpResponse<T>> token)
         {
-            var httpResponse = new HttpResponse<T>
+            while (!request.isDone)
             {
-                Headers = FormatHeaders(response.Headers),
-                StatusCode = (long)response.StatusCode
-            };
-
-            using (var stream = response.GetResponseStream())
-            {
-                if (null == stream)
-                {
-                    token.Fail(new Exception("Null response."));
-
-                    return;
-                }
-
-                // TODO: reuse buffers
-                var bytes = new byte[16384];
-                var index = 0;
-                int bytesRead;
-
-                while ((bytesRead = stream.Read(bytes, index, bytes.Length - index)) > 0)
-                {
-                    index += bytesRead;
-
-                    if (index == bytes.Length)
-                    {
-                        var newBuffer = new byte[bytes.Length * 2];
-                        Array.Copy(bytes, 0, newBuffer, 0, index);
-                        bytes = newBuffer;
-                    }
-                }
-
-                object value = null;
-                switch (serialization)
-                {
-                    case SerializationType.Json:
-                        {
-                            try
-                            {
-                                _serializer.Deserialize(typeof(T), ref bytes, out value);
-                            }
-                            catch (Exception exception)
-                            {
-                                httpResponse.NetworkSuccess = false;
-                                httpResponse.NetworkError = string.Format("Could not deserialize : {0}.", exception.Message);
-                            }
-                            break;
-                        }
-                    default:
-                        {
-                            value = bytes;
-                            break;
-                        }
-                }
-
-                httpResponse.Payload = (T)value;
-
-                if (Successful((int) httpResponse.StatusCode))
-                {
-                    httpResponse.NetworkSuccess = true;
-                }
-                else
-                {
-                    httpResponse.NetworkSuccess = false;
-                    httpResponse.NetworkError = Encoding.UTF8.GetString(bytes);
-                }
+                yield return null;
             }
 
-            response.Close();
+            var httpResponse = new HttpResponse<T>
+            {
+                StatusCode = GetStatusCode(request),
+                Headers = null == request.responseHeaders
+                    ? new List<Tuple<string, string>>()
+                    : request
+                        .responseHeaders
+                        .Select(pair => Tuple.Create(pair.Key, pair.Value))
+                        .ToList(),
+                Raw = request.bytes
+            };
+
+            var bytes = request.bytes;
+
+            object value = null;
+            if (typeof(T) != typeof(byte[]))
+            {
+                try
+                {
+                    _serializer.Deserialize(typeof(T), ref bytes, out value);
+                }
+                catch (Exception exception)
+                {
+                    httpResponse.NetworkSuccess = false;
+                    httpResponse.NetworkError = string.Format("Could not deserialize : {0}.", exception.Message);
+                }
+            }
+            else
+            {
+                value = bytes;
+            }
+
+            httpResponse.Payload = (T)value;
+
+            if (Successful((int)httpResponse.StatusCode))
+            {
+                httpResponse.NetworkSuccess = true;
+            }
+            else
+            {
+                httpResponse.NetworkSuccess = false;
+                httpResponse.NetworkError = Encoding.UTF8.GetString(bytes);
+            }
 
             token.Succeed(httpResponse);
         }
 
         /// <summary>
-        /// Applies headers to request.
+        /// Figures out the status code.
         /// </summary>
-        /// <param name="headers">Headers to add to request.</param>
-        /// <param name="request">Request to add headers to.</param>
-        protected void ApplyHeaders(
-            List<Tuple<string, string>> headers,
-            HttpWebRequest request)
+        /// <param name="request">Request.</param>
+        /// <returns></returns>
+        private int GetStatusCode(WWW request)
         {
-            if (headers == null)
+            var headers = request.responseHeaders;
+            if (null == headers)
             {
-                return;
+                return 0;
             }
 
-            for (int i = 0, len = headers.Count; i < len; i++)
+            if (headers.ContainsKey("STATUS"))
             {
-                var kv = headers[i];
-                if (!WebHeaderCollection.IsRestricted(kv.Item1))
+                var status = headers["STATUS"];
+                var substrings = status.Split(' ');
+                if (substrings.Length < 3)
                 {
-                    request.Headers.Add(kv.Item1, kv.Item2);
+                    Log.Warning(this, "Invalid response status: {0}.", status);
                 }
                 else
                 {
-                    Log.Warning(this, "Header [{0}] is restricted.", kv.Item1);
+                    int returnValue;
+                    if (!int.TryParse(substrings[1], out returnValue))
+                    {
+                        Log.Error("Invalid response code: {0}.", substrings[1]);
+                        return 0;
+                    }
+
+                    return returnValue;
                 }
             }
 
-            request.ContentType = CONTENT_TYPE_JSON;
-            request.Accept = CONTENT_TYPE_JSON;
+            return 0;
         }
 
         /// <summary>
-        /// Applies json payload to request.
+        /// Creates a dictionary from headers.
         /// </summary>
-        /// <param name="payload">The payload to add to the request.</param>
-        /// <param name="request">The request.</param>
-        protected void ApplyJsonPayload(
-            object payload,
-            HttpWebRequest request)
+        /// <returns></returns>
+        private Dictionary<string, string> HeaderDictionary()
         {
-            if (null == payload)
+            var dictionary = new Dictionary<string, string>();
+
+            foreach (var pair in Headers)
             {
-                return;
+                dictionary[pair.Item1] = pair.Item2;
             }
 
-            byte[] payloadBytes;
+            dictionary["Content-Type"] = CONTENT_TYPE_JSON;
+            dictionary["Accept"] = CONTENT_TYPE_JSON;
 
-            // let serialization errors bubble up
-            _serializer.Serialize(payload, out payloadBytes);
-
-            using (var stream = request.GetRequestStream())
-            {
-                stream.Write(payloadBytes, 0, payloadBytes.Length);
-            }
+            return dictionary;
         }
-        
+
         /// <summary>
         /// Returns true iff status code indicates a success.
         /// </summary>
@@ -321,28 +266,6 @@ namespace CreateAR.Commons.Unity.Http.Editor
         {
             // any 200 is a success
             return statusCode >= 200 && statusCode < 300;
-        }
-
-        /// <summary>
-        /// Converts headers to a list of CreateAR.Commons.Unity.DataStructures.Tuples.
-        /// </summary>
-        /// <param name="headers">The header collection.</param>
-        /// <returns>A list of CreateAR.Commons.Unity.DataStructures.Tuples that represent the headers.</returns>
-        private static List<Tuple<string, string>> FormatHeaders(WebHeaderCollection headers)
-        {
-            var tuples = new List<Tuple<string, string>>();
-
-            if (null != headers)
-            {
-                foreach (var key in headers.AllKeys)
-                {
-                    tuples.Add(Tuple.Create(
-                        key,
-                        headers[key]));
-                }
-            }
-
-            return tuples;
         }
     }
 }
