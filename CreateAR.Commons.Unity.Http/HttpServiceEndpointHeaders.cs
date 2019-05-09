fba2354386f82899e63e8f3e9a4f6449e0b05fd6
Matt Bolt <mbolt35@gmail.com>
2019-05-09 16:46:01 -0400

    Get off my back CodeFactor

---
+++ CreateAR.Commons.Unity.Http/HttpServiceEndpointHeaders.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace CreateAR.Commons.Unity.Http
+{
+    /// <summary>
+    /// This type contains a service endpoint specific set of headers.
+    /// </summary>
+    public class HttpServiceEndpointHeaders
+    {
+        /// <summary>
+        /// The name of the endpoint to use the included headers for.
+        /// </summary>
+        public string EndpointName { get; }
+
+        /// <summary>
+        /// Headers used for a specific endpoint.
+        /// </summary>
+        public Dictionary<string, string> Headers { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="HttpServiceHeaders"/> instance.
+        /// </summary>
+        /// <param name="serviceName"></param>
+        public HttpServiceEndpointHeaders(string endpointName)
+        {
+            EndpointName = endpointName;
+            Headers = new Dictionary<string, string>();
+        }
+    }
+}
